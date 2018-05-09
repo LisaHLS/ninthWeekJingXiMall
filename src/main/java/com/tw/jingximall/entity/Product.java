ded2909815a62d37a59a92b0c2a4dacc2a3cc882
LisaHuang <1449700087@qq.com>
2018-05-09 19:39:13 +0800

    implement entity Product.java

--- src/main/java/com/tw/jingximall/entity/Product.java
+++ src/main/java/com/tw/jingximall/entity/Product.java
@@ -1,6 +1,34 @@
 package com.tw.jingximall.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name ="Product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private Integer price;
+
+    @OneToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER, mappedBy = "product")
+    private Inventory inventory;
 }
