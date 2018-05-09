8fdab16e02b9aba16da7315e8e34117751a896a3
LisaHuang <1449700087@qq.com>
2018-05-09 22:03:11 +0800

    feat: create interface ProductRepository

---
+++ src/main/java/com/tw/jingximall/repository/ProductRepository.java
@@ -0,0 +1,36 @@
+package com.tw.jingximall.repository;
+
+import com.tw.jingximall.entity.Product;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    //创建新商品
+    @Transactional
+    Product save(Product product);
+
+    //修改商品信息
+    @Transactional
+    @Modifying
+    @Query("update Product set name = ?2, description = ?3, price = ?4 where id = ?1")
+    int update(Integer id, String name, String description, Integer price);
+
+    //根据商品id查找商品
+    Product findProductById(Long id);
+
+    //查找所有商品
+    List<Product> findAll();
+
+    //根据name查询
+    List<Product> findByName(String name);
+
+    //根据name和描述模糊查询
+    List<Product> findByNameAndDescriptionContaining(String name, String description);
+
+}
