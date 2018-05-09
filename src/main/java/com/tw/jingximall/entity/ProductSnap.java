2259a47f085a525e1cdf39ca22a448e74ab2238a
LisaHuang <1449700087@qq.com>
2018-05-09 20:36:46 +0800

    create and implement entity ProductSnap.java

---
+++ src/main/java/com/tw/jingximall/entity/ProductSnap.java
@@ -0,0 +1,40 @@
+package com.tw.jingximall.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductSnap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer productId;
+
+    private Integer orderId;
+
+    private String productName;
+
+    private String productDescription;
+
+    private Integer purchasePrice;
+
+    private Integer purchaseCount;
+
+    @ManyToOne(targetEntity = Order.class)
+    @JoinColumn(name ="orderId", insertable = false, updatable = false)
+    private Order orderItem;
+
+}
