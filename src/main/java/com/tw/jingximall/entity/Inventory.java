6983e7ee4f0da56fde7066d16b861fb0469b6e1d
LisaHuang <1449700087@qq.com>
2018-05-09 19:45:11 +0800

    implement entity Inventory.java

--- src/main/java/com/tw/jingximall/entity/Inventory.java
+++ src/main/java/com/tw/jingximall/entity/Inventory.java
@@ -1,5 +1,30 @@
 package com.tw.jingximall.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="Inventory")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventory {
+
+    @Id
+    private Long id;
+
+    private Integer count;
+
+    private Integer lockedCount;
+
+    @OneToOne(targetEntity = Product.class)
+    @JoinColumn(name = "id", insertable = false, updatable = false)
+    private Product product;
 
 }
