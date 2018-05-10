17c3ec6270f4ad0481509e430c5201ae4636913a
LisaHuang <1449700087@qq.com>
2018-05-10 13:42:33 +0800

    fix: add @Repository

--- src/main/java/com/tw/jingximall/repository/InventoryRepository.java
+++ src/main/java/com/tw/jingximall/repository/InventoryRepository.java
@@ -6,7 +6,9 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
     //创建库存
