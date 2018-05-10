258cb9a71481d3ebc7d43c05271df7ec9a3ea0bd
LisaHuang <1449700087@qq.com>
2018-05-10 12:50:16 +0800

    feat: add Inventory API

--- src/main/java/com/tw/jingximall/repository/InventoryRepository.java
+++ src/main/java/com/tw/jingximall/repository/InventoryRepository.java
@@ -1,6 +1,7 @@
 package com.tw.jingximall.repository;
 
 import com.tw.jingximall.entity.Inventory;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,5 +14,23 @@ public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
     @Transactional
     @Query(value = "insert into Inventory(id,count,lockedCount) values(?1,0,0)",nativeQuery = true)
     int saveByProductId(Integer id);
+
+    //修改库存数量
+    @Modifying
+    @Transactional
+    @Query("update Inventory set count = count + ?2 where id = ?1")
+    int updateCountById(Integer productId, Integer count);
+
+    //修改锁定库存数量
+    @Modifying
+    @Transactional
+    @Query("update Inventory set lockedCount = lockedCount + ?2 where id = ?1")
+    int updateLockedCount(Integer productId, Integer lockedCount);
+
+    //根据商品id查找库存
+    Inventory findInventoryById(Integer productId);
+
+    //查找所有库存
+    List<Inventory> findAll();
 
 }
