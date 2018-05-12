351f35f6765a982af7dd1619757bf61d9525ece9
LisaHuang <1449700087@qq.com>
2018-05-12 09:43:57 +0800

    fix:org.hibernate.exception.SQLGrammarException: could not execute query

--- src/main/java/com/tw/jingximall/entity/OrderInfo.java
+++ src/main/java/com/tw/jingximall/entity/OrderInfo.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="OrderInfo")
 public class OrderInfo {
 
     @Id
--- src/main/java/com/tw/jingximall/entity/ProductSnap.java
+++ src/main/java/com/tw/jingximall/entity/ProductSnap.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="ProductSnap")
 public class ProductSnap {
 
     @Id
